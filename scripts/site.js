e989f1068d988faa4f816dbba78ac0386af4cad2
vamseemanchineni <vamsimanchineni26@gmail.com>
2024-07-15 22:53:25 +0530

    Section 6

--- scripts/site.js
+++ scripts/site.js
@@ -14,3 +14,14 @@ function HidePTags(){
 $('#jqueryBtn').click(function(){
    $('p').hide();
 })
+
+$(function(){
+   var pages=['index','about','contact'];
+   var pathname=window.location.pathname;
+   $('.nav-link').each(function(item){
+      if(pathname.includes(pages[item])){
+         $(this).addClass('active');
+         $(this).attr('aria-current','page');
+      }
+   })
+});
